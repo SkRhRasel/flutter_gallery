bba5ab41ca4017174e506d9d6d3089f479dc0c58
SkRhRasel <sk.rasel.cse@gmail.com>
2022-02-04 22:53:34 +0600

    home bloc class

--- lib/home/bloc/home_bloc.dart
+++ lib/home/bloc/home_bloc.dart
@@ -1,15 +1,39 @@
-import 'dart:async';
-
 import 'package:bloc/bloc.dart';
+import 'package:connectivity_plus/connectivity_plus.dart';
 import 'package:equatable/equatable.dart';
+import 'package:flutter_gallery/home/services/connectivityService.dart';
+import 'package:flutter_gallery/home/services/picsumPhotosService.dart';
 
 part 'home_event.dart';
+
 part 'home_state.dart';
 
 class HomeBloc extends Bloc<HomeEvent, HomeState> {
-  HomeBloc() : super(HomeInitial()) {
-    on<HomeEvent>((event, emit) {
-      // TODO: implement event handler
+  final PicsumPhotosService _picsumPhotosService;
+  final ConnectivityService _connectivityService;
+
+  HomeBloc(this._picsumPhotosService, this._connectivityService) : super(HomeLoadingState()) {
+
+    _connectivityService.connectivityStream.stream.listen((event) {
+      if (event == ConnectivityResult.none) {
+        add(NoInternetEvent());
+        print('No Internet');
+        showToast('No Internet');
+      }else{
+        print('Yes Internet');
+        add(LoadApiEvent());
+      }
+    });
+
+
+    on<LoadApiEvent>((event, emit) async {
+      emit(HomeLoadingState());
+      final activity = await _picsumPhotosService.getPicsumPhotosActivity();
+      emit(HomeLoadedState(activity));
+    });
+
+    on<NoInternetEvent>((event, emit){
+      emit(HomeNoInternetState());
     });
   }
 }
