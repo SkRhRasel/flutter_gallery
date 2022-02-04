1a4f6a8afab9c6c92a67ba0c681c7362f9650f3d
SkRhRasel <sk.rasel.cse@gmail.com>
2022-02-04 22:54:18 +0600

    home state class

--- lib/home/bloc/home_state.dart
+++ lib/home/bloc/home_state.dart
@@ -4,7 +4,33 @@ abstract class HomeState extends Equatable {
   const HomeState();
 }
 
-class HomeInitial extends HomeState {
+class HomeLoadingState extends HomeState {
   @override
   List<Object> get props => [];
+}
+
+class HomeLoadedState extends HomeState{
+
+  // final String id;
+  // final String author;
+  // final String downloadUrl;
+  List<PicsumPhotosActivity> picsumPhotosList;
+
+
+  //HomeLoadedState(this.id, this.author, this.downloadUrl);
+  HomeLoadedState(this.picsumPhotosList);
+
+
+  @override
+  // TODO: implement props
+  //List<Object?> get props => [id,author,downloadUrl];
+  List<Object?> get props => [picsumPhotosList];
+
+}
+
+class HomeNoInternetState extends HomeState{
+  @override
+  // TODO: implement props
+  List<Object?> get props => [];
+
 }
